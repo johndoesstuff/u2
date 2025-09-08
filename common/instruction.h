57128d9c2f57dda214894d886fc0287768ca4a81
johndoesstuff <johndoesstuff03@gmail.com>
2025-09-08 16:45:56 -0400

    inst

--- common/instruction.h
+++ common/instruction.h
@@ -3,12 +3,12 @@
 */
 
 enum {
-	FORMAT_F, // 3 register :		ins r0 r1 r2
-	FORMAT_R, // register to register :	ins r0 r1
-	FORMAT_I, // immediate to register :	ins r0 63
-	FORMAT_J, // immediate opcode :		ins 63
-	FORMAT_D, // register opcode : 		ins r0
-	FORMAT_NONE, // opcode :		ins
+	FORMAT_F,    // 3 register :            ins r0 r1 r2
+	FORMAT_R,    // register to register :  ins r0 r1
+	FORMAT_I,    // immediate to register : ins r0 im
+	FORMAT_J,    // immediate opcode :      ins im
+	FORMAT_D,    // register opcode :       ins r0
+	FORMAT_NONE, // opcode :                ins
 } typedef InstructionFormat;
 
 struct {
@@ -17,8 +17,10 @@ struct {
 } typedef Instruction;
 
 Instruction Instructions[] = {
-	{ FORMAT_R, "mov" },
-	{ FORMAT_D, "dref" },
+	{ FORMAT_R, "mov" },    // copy rs1 to rd
+	{ FORMAT_I, "li" },     // load immediate to rd
+	{ FORMAT_R, "ld" },     // load memory to rd
+	{ FORMAT_R, "st" },     // store to memory from rd
 };
 
 int Instruction_Count = sizeof(Instructions)/sizeof(Instructions[0]);
