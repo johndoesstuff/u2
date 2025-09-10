caad60ef7c62b92e488362a4747298a66bafb985
johndoesstuff <johndoesstuff03@gmail.com>
2025-09-10 18:52:06 -0400

    jit prog

--- vm/x86jit.c
+++ vm/x86jit.c
@@ -23,15 +23,69 @@
  */
 
 #include "x86jit.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int VMRegMap[16] = {
+	X86_RAX,
+	X86_RCX,
+	X86_RDX,
+	X86_RSI,
+	X86_RDI,
+	X86_R8,
+	X86_R9,
+	X86_R10,
+	X86_R11,
+	X86_SPILL,
+	X86_SPILL,
+	X86_SPILL,
+	X86_SPILL,
+	X86_SPILL,
+	X86_SPILL,
+	X86_SPILL,
+};
+
+uint64_t VMRegSpill[16] = {0};
+
 void emit_byte(char** jit_memory, unsigned char byte) {
 	*(*jit_memory)++ = byte;
 }
 
+void emit_rex(char **buf, int w, int reg, int rm) {
+	uint8_t rex = REX_BASE;
+	if (w) rex |= REX_W;       // 64-bit
+	if (reg & 8) rex |= REX_R; // R
+	if (rm  & 8) rex |= REX_B; // B
+	if (rex != REX_BASE) emit_byte(buf, rex);
+}
+
 void emit_mov(char** jit_memory, unsigned int rd, unsigned int rs1) {
-	// TODO: make this work lol
-	emit_byte(jit_memory, REXW);
-	emit_byte(jit_memory, OPCODE_MOV_REG_REG);
-	return;
+	int dst = VMRegMap[rd];
+	int src = VMRegMap[rs1];
+
+	// register to register
+	if (dst != X86_SPILL && src != X86_SPILL) {
+		emit_rex(jit_memory, 1, src, dst);
+		emit_byte(jit_memory, OPCODE_MOV_REG_REG);
+		emit_byte(jit_memory, MODRM(0b11, src, dst));
+	} else {
+		printf("TODO: implement this lol\n");
+		exit(1);
+	}
+
+	// TODO: implement spill
+}
+
+void emit_li(char** jit_memory, unsigned int rd, unsigned int imm) {
+	// TODO: implement li
+	int dst = VMRegMap[rd];
+	
+	if (dst != X86_SPILL) {
+		// emit mov imm
+	} else {
+		printf("TODO: implement this aswell\n");
+		exit(1);
+	}
 }
 
 void emit_jit(char** jit_memory,
@@ -43,6 +97,9 @@ void emit_jit(char** jit_memory,
 	switch (opcode) {
 		case 0:
 			emit_mov(jit_memory, rd, rs1);
+			break;
+		case 1:
+			emit_li(jit_memory, rd, imm);
 			break;
 	}
 }
--- vm/x86jit.h
+++ vm/x86jit.h
@@ -1,11 +1,13 @@
 #ifndef X86JIT_H
 #define X86JIT_H
 
+#include <stdint.h>
+
 #define REX_BASE   0x40
 #define REX_W      0x08
 #define REX_R      0x04
 #define REX_X      0x02
-#define REX_B
+#define REX_B      0x01
 
 #define REXW       (REX_BASE | REX_W)
 
@@ -15,6 +17,26 @@
 #define OPCODE_RET          0xC3
 
 #define MODRM(mod, reg, rm) ((uint8_t)(((mod) << 6) | (((reg) & 7) << 3) | ((rm) & 7)))
+
+enum X86Reg {
+	X86_SPILL = -1,
+	X86_RAX = 0,
+	X86_RCX = 1,
+	X86_RDX = 2,
+	X86_RBX = 3,
+	X86_RSP = 4, // reserved
+	X86_RBP = 5, // reserved
+	X86_RSI = 6,
+	X86_RDI = 7,
+	X86_R8  = 8,
+	X86_R9  = 9,
+	X86_R10 = 10,
+	X86_R11 = 11,
+	X86_R12 = 12,
+	X86_R13 = 13,
+	X86_R14 = 14,
+	X86_R15 = 15
+};
 
 void emit_jit(char** jit_memory, unsigned int opcode, unsigned int rd, unsigned int rs1, unsigned int rs2, unsigned int imm);
 
